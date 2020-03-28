3bdac07d51e3a29d247a3323af7f541e1d50bd12
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-29 01:03:56 +0530

    help command placeholder done

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-//Adding Discord
+//?Adding Discord
 const Discord = require("discord.js");
-const { prefix: pre, token } = require("./config.json");
+const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
 
+//? Adding utilities
+const basic = require("./utils/basic");
+
+//?Code for bot
 client.once("ready", () => {
   console.log("Ready!");
 });
 
-client.on("message", msg => {});
+//? help command
+client.on("message", msg => {
+  if (msg.content.startsWith(`${prefix}help`)) {
+    basic.help(help => {
+      msg.channel.send(help);
+    });
+  }
+});
 
-//This statement makes the bot work
+//?This statement makes the bot work
 client.login(token);
--- utils/basic.js
+++ utils/basic.js
@@ -1,1 +1,9 @@
 //This file contains the basic command that anyone can use
+const help = callback => {
+  const msg = `:warning: Under Maintainence :warning:`;
+  callback(msg);
+};
+
+module.exports = {
+  help
+};
