5ceb3e18a5f166111f76e7a822b6153b1e20908b
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-29 01:21:11 +0530

    added clear command(TBR)

--- index.js
+++ index.js
@@ -8,16 +8,27 @@ const basic = require("./utils/basic");
 
 //?Code for bot
 client.once("ready", () => {
-  console.log("Ready!");
+	console.log("Ready!");
 });
 
 //? help command
 client.on("message", msg => {
-  if (msg.content.startsWith(`${prefix}help`)) {
-    basic.help(help => {
-      msg.channel.send(help);
-    });
-  }
+	if (msg.content.startsWith(`${prefix}help`)) {
+		basic.help(help => {
+			msg.channel.send(help);
+		});
+	}
+	//? clear command
+	//TODO review the below command
+	if (msg.content.startsWith(`${pre}clear`)) {
+		try {
+			let count = msg.content.split(" ")[1];
+			msg.channel.bulkDelete(count);
+		} catch (e) {
+			msg.channel.send("Oops something went wrong");
+			console.log(e);
+		}
+	}
 });
 
 //?This statement makes the bot work
