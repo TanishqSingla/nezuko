da0f7dc6dfc90394ad2e310bddcd75a0ba61efd8
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-29 15:41:02 +0530

    completed word filter

--- index.js
+++ index.js
@@ -8,32 +8,37 @@ const basic = require("./utils/basic");
 
 //?Code for bot
 client.once("ready", () => {
-	console.log("Ready!");
+  console.log("Ready!");
 });
 
 //? help command
 client.on("message", msg => {
-	const author = msg.author;
-	if (msg.content.startsWith(`${prefix}help`)) {
-		basic.help(help => {
-			msg.channel.send(help);
-		});
-	}
-	//? clear command
-	if (msg.content.startsWith(`${prefix}clear`)) {
-		try {
-			let count = msg.content.split(" ")[1];
-			msg.channel.bulkDelete(count);
-		} catch (e) {
-			msg.channel.send("Oops something went wrong");
-			console.log(e);
-		}
-	}
-	//? <----- Word filter
-	if (basic.badWords.some(w => msg.content.toLowerCase().includes(w))) {
-		msg.delete();
-	}
-	//? -------->
+  const author = msg.author;
+  if (msg.content.startsWith(`${prefix}help`)) {
+    basic.help(help => {
+      msg.channel.send(help);
+    });
+  }
+  //? clear command
+  if (msg.content.startsWith(`${prefix}clear`)) {
+    try {
+      let count = msg.content.split(" ")[1];
+      msg.channel.bulkDelete(count);
+    } catch (e) {
+      msg.channel.send("Oops something went wrong");
+      console.log(e);
+    }
+  }
+  //? <----- Word filter
+  if (basic.badWords.some(w => msg.content.toLowerCase().includes(w))) {
+    msg.delete();
+    msg.channel.send(`Watch your language ${author}`).then(msg => {
+      setTimeout(() => {
+        msg.delete();
+      }, 2000);
+    });
+  }
+  //? -------->
 });
 
 //?This statement makes the bot work
